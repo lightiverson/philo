7b3b5f542d87c4965c037cee262c7a6b76bfeadb
Kawish Gajadien <kawishg@outlook.com>
2023-02-16 15:16:31 +0100

    fix: pas volgorde van vorken pakken en neerzetten aan om helgrind errors te fixen

--- src/state_cont.c
+++ src/state_cont.c
@@ -6,7 +6,7 @@
 /*   By: kgajadie <kgajadie@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/01/12 15:13:45 by kgajadie      #+#    #+#                 */
-/*   Updated: 2023/02/14 14:30:29 by kgajadie      ########   odam.nl         */
+/*   Updated: 2023/02/16 14:07:47 by kgajadie      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 void	take_forks(t_philo *philo)
 {
-	pthread_mutex_lock(philo->left_fork);
 	pthread_mutex_lock(philo->right_fork);
+	pthread_mutex_lock(philo->left_fork);
 }
 
 void	put_forks(t_philo *philo)
 {
-	pthread_mutex_unlock(philo->left_fork);
 	pthread_mutex_unlock(philo->right_fork);
+	pthread_mutex_unlock(philo->left_fork);
 }
 
 pthread_mutex_t	*left(t_philo *philo, int i)
